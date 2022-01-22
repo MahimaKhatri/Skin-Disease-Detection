cbb1bcb17c53dcf90bc3469e3f46d96035f9bb7d
Mahima Khatri <mahima8301@gmail.com>
2022-01-22 23:01:15 +0530

    Work In Progress

---
+++ Front-end/main.js
@@ -0,0 +1,32 @@
+let menu = document.querySelector('#menu-bar');
+let nav = document.querySelector('.nav');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    nav.classList.toggle('active');
+}
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .nav a');
+
+window.onscroll = () =>{
+
+    menu.classList.remove('fa-times');
+    nav.classList.remove('active');
+
+    section.forEach(sec =>{
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop - 150;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header .nav a[href*='+id+']').classList.add('active');
+            });
+        };
+    });
+
+}

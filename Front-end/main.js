b84b430d1af30561014aaac9df4650a49066f07f
Mahima Khatri <mahima8301@gmail.com>
2022-01-23 01:16:04 +0530

    Front End

--- Front-end/main.js
+++ Front-end/main.js
@@ -30,3 +30,21 @@ window.onscroll = () =>{
     });
 
 }
+
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function() {
+  realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+  if (realFileBtn.value) {
+    customTxt.innerHTML = realFileBtn.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    customTxt.innerHTML = "No file chosen, yet.";
+  }
+});
